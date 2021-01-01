da924d90873f11a335d58f10833d5ff49b9b7a0b
Chow Jia Ying <chowjiaying211@gmail.com>
2021-01-01 16:31:36 +0800

    Add styling for underline icon

--- src/index.js
+++ src/index.js
@@ -6,11 +6,36 @@
  *
  */
 
+import "./index.css"
+
+function initUI(editor) {
+  const toolbar = editor.getUI().getToolbar()
+  const italicIndex = toolbar.indexOfItem("italic")
+
+  toolbar.insertItem(italicIndex + 1, {
+    type: "button",
+    options: {
+      name: "underline",
+      className: "tui-underline",
+      event: "underline",
+      tooltip: "Underline",
+    },
+  })
+}
+
 /**
  * Underline plugin
  * Some code was modified from https://github.com/nhn/tui.editor/blob/master/plugins/color-syntax/src/js/index.js to learn how to make use of the TUI Editor's API to create the plugin
  * @param {Editor|Viewer} editor - instance of Editor or Viewer
  */
 
-export default function underlinePlugin(editor) {
+export default function underline(editor) {
+  // add commands for editor
+  initUI(editor)
+  editor.addCommand("markdown", {
+    name: "underline",
+    exec(md) {
+      console.log(md)
+    },
+  })
 }
